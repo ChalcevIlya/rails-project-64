440fcacd7eb71a2c2cb803efffa94d904b6aea0f
Chalcev Ilya <mr.chalcev@bk.ru>
2025-06-16 18:38:33 +0300

    Final linter corrections

--- db/migrate/20250616134247_add_unique_index_to_post_likes_user_id_and_post_id.rb
+++ db/migrate/20250616134247_add_unique_index_to_post_likes_user_id_and_post_id.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class AddUniqueIndexToPostLikesUserIdAndPostId < ActiveRecord::Migration[7.2]
   def change
-    add_index :post_likes, [:user_id, :post_id], unique: true
+    add_index :post_likes, %i[user_id post_id], unique: true
   end
 end
--- db/migrate/20250616140013_rename_column_body_to_content_in_table_post_comments.rb
+++ db/migrate/20250616140013_rename_column_body_to_content_in_table_post_comments.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameColumnBodyToContentInTablePostComments < ActiveRecord::Migration[7.2]
   def change
     rename_column :post_comments, :body, :content
